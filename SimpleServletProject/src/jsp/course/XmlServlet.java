539f2507ad47bed186dcea011f28872b9f6e08fd
aprilzhang <aprilsulu@gmail.com>
2016-08-14 00:51:22 +0100

    Passing parameters.

--- SimpleServletProject/src/jsp/course/XmlServlet.java
+++ SimpleServletProject/src/jsp/course/XmlServlet.java
@@ -1,6 +1,7 @@
 package jsp.course;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,21 @@ public class XmlServlet extends HttpServlet {
 	@Override
 	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("XML servlet called!");
+		response.setContentType("text/html");
+		final PrintWriter out = response.getWriter();
+		final String userName = request.getParameter("userName");
+		final String userId = request.getParameter("userId");
+		out.println("Hello from GET method! " + userName + ":" + userId + "!");
 	}
 
+	@Override
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		final PrintWriter out = response.getWriter();
+		final String userName = request.getParameter("userName");
+		final String userId = request.getParameter("userId");
+		out.println("Hello from POST method!" + userName + ":" + userId + "!");
+	}
 }
